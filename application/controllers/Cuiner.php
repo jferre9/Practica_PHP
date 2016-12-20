167e2ffb05d4ebae521695202e2ec989c8351c6f
joan <joan@joan-PC>
2016-12-20 21:33:01 +0100

    a

--- application/controllers/Cuiner.php
+++ application/controllers/Cuiner.php
@@ -1,8 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Cuiner extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+    }
 	
 	public function index()
 	{
--- application/models/user.php
+++ application/models/user.php
@@ -1,27 +1,29 @@
 <?php
-class User extends CI_Model {
 
-  public $nom;
-  
+class Usuari extends CI_Model {
 
-  public function __construct()
-  {
-          // Call the CI_Model constructor
-          parent::__construct();
-  }
+    public $id;
+    public $email;
+    public $nom;
+    public $cognoms;
+    public $pass;
+    public $cambrer;
+    public $cuiner;
+    public $cobrar;
+    
 
- 
+    public function __construct() {
+        // Call the CI_Model constructor
+        parent::__construct();
+    }
 
-  public function insert_entry($nom)
-  {
-    $this->nom    = $nom; 
-    $this->db->insert('user', $this);
-  }
-  
-  public function elimina($id)
-  {
-    $this->db->delete('user', array('id' => $id));
-  }
+    public function insert_entry($data) {
+        $data['pass'] = md5($data['pass']);
+        $this->db->insert('usuari', $data);
+    }
 
+    public function elimina($id) {
+        $this->db->delete('user', array('id' => $id));
+    }
 
 }
